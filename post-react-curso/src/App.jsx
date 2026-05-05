69e6bdbf1c5506aedb444426656e34c29965d352
kuisval <L22170842@culiacan.tecnm.mx>
2026-05-04 18:21:05 -0700

    status: inicializacion de proyecto nuevo para practicar.

---
+++ post-react-curso/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <span>Hola Mundo</span>
+    </>
+  )
+}
+
+export default App
